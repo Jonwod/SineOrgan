4c12f15984473df3c8ec8e32a3796409db25027d
Jon <jonathan.m.wood95@gmail.com>
2019-09-22 16:29:31 +0100

    Added equal temperament tunings and refactored organ slightly to use a function pointer to a frequency function.

--- Concrete/SineOrgan.cpp
+++ Concrete/SineOrgan.cpp
@@ -8,13 +8,39 @@
 #include "../Lib/WaveMath.h"
 
 
-SineOrgan::SineOrgan(int numKeys) {
-    _keyStates = std::vector<bool>(numKeys, false);
+// ~~~~~~~~ Frequency Functions ~~~~~~~~~~
+double equalTemperament(unsigned int octaveDiv, double baseFreq, int note) {
+    // Possible optimization: this only needs to be calculated once for each temperament system. Perhaps a functor?
+    const double intervalRatio = pow(2.0, 1.0/octaveDiv);
+
+    return pow(intervalRatio, note) * baseFreq;
 }
 
 
-double SineOrgan::freqOfKey(int key) const {
-    return _baseFrequency + key * _baseFrequency * 1.5;
+double equalTemperament12(int note) {
+    return equalTemperament(12, 200.0, note);
+}
+
+
+double equalTemperament5(int note) {
+    return equalTemperament(5, 120.0, note);
+}
+
+double equalTemperament7(int note) {
+    return equalTemperament(7, 120.0, note);
+}
+
+
+double weirdFreqFunc1(int key) {
+    const double baseFrequency = 40.0;
+    return baseFrequency + key * baseFrequency * 1.5;
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+SineOrgan::SineOrgan(int numKeys) {
+    _keyStates = std::vector<bool>(numKeys, false);
 }
 
 
@@ -24,7 +50,7 @@ void SineOrgan::updateBufferDerived(unsigned int startSample) {
     // TODO: Placeholder just plays tone if any key is pressed.
     for(int i = 0; i < _keyStates.size(); ++i) {
         if(_keyStates[i]) {
-            const auto freq = freqOfKey(i);
+            const auto freq = _frequencyFunction(i);
             WaveMath::addSine(_privateBuffer.data(), _privateBuffer.size(),
                     getSampleRate(),freq, std::numeric_limits<SampleType>::max() / _maxNotes, startSample);
         }
--- Concrete/SineOrgan.h
+++ Concrete/SineOrgan.h
@@ -6,8 +6,22 @@
 #define SINEORGAN_SINEORGAN_H
 
 #include <vector>
+#include <cmath>
 #include "../Base/InstrumentBase.h"
 #include "../Interface/OrganInterface.h"
+
+
+// ~~~~~~~~ Frequency Functions ~~~~~~~~~~
+double equalTemperament(unsigned int octaveDiv, double baseFreq, int note);
+
+double equalTemperament12(int note);
+
+double equalTemperament5(int note);
+
+double equalTemperament7(int note);
+
+double weirdFreqFunc1(int note);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class SineOrgan: public InstrumentBase, public OrganInterface {
@@ -21,13 +35,16 @@ public:
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     unsigned int numKeys() const override { return _keyStates.size();}
+
+    // A frequency function takes an integer corresponding to a key and returns
+    // a frequency
+    typedef double (*FrequencyFunction)(int);
 protected:
     void updateBufferDerived(unsigned int startSample) override;
 private:
-    // Calculates the fundamental frequency for the specified key.
-    double freqOfKey(int key) const;
+    FrequencyFunction _frequencyFunction = equalTemperament7;
+
     std::vector<bool> _keyStates;
-    const double _baseFrequency = 40.0;
     const int _maxNotes = 5;
 };
 
